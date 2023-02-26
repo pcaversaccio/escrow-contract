298d566ef0118f29ecdb8d1476765029cd7d6b4b
Pascal Marco Caversaccio <pascal.caversaccio@hotmail.ch>
2023-02-26 13:48:15 +0100

    ♻️  bump deps and small refactors
    Signed-off-by: Pascal Marco Caversaccio <pascal.caversaccio@hotmail.ch>

--- contracts/CobieEscrow.sol
+++ contracts/CobieEscrow.sol
@@ -1,9 +1,9 @@
 // SPDX-License-Identifier: WTFPL
 pragma solidity 0.8.19;
 
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
-import "@openzeppelin/contracts/access/AccessControl.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import {AccessControl} from "@openzeppelin/contracts/access/AccessControl.sol";
 
 /////////////////////////////////////////////////////////////////
 //////////                CUSTOM ERRORS                //////////
