8f7067dbf84dcbfb2452545c74a3978a09283d79
Pascal Marco Caversaccio <pascal.caversaccio@hotmail.ch>
2023-11-24 14:22:04 +0100

    ♻️  Refactor Deployment Scripts
    Signed-off-by: Pascal Marco Caversaccio <pascal.caversaccio@hotmail.ch>

--- deploy/deploy-zksync.ts
+++ deploy/deploy-zksync.ts
@@ -5,15 +5,27 @@ import { Deployer } from "@matterlabs/hardhat-zksync-deploy";
 
 const PRIVATE_KEY: string = process.env.PRIVATE_KEY || "";
 
+function delay(ms: number) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
 export default async function main(hre: HardhatRuntimeEnvironment) {
-  const cobie = "0x4Cbe68d825d21cB4978F56815613eeD06Cf30152";
+  const cobie = ["0x4Cbe68d825d21cB4978F56815613eeD06Cf30152"];
   const wallet = new Wallet(PRIVATE_KEY);
   const deployer = new Deployer(hre, wallet);
 
   const artifact = await deployer.loadArtifact("CobieEscrow");
-  const contract = await deployer.deploy(artifact, [cobie]);
+  const contract = await deployer.deploy(artifact, cobie);
 
   await contract.deployed();
+  const contractAddress = contract.address;
 
-  console.log("CobieEscrow deployed to:", contract.address);
+  console.log("CobieEscrow deployed to:", contractAddress);
+
+  await delay(30000); // Wait for 30 seconds before verifying the contract
+
+  await hre.run("verify:verify", {
+    address: contractAddress,
+    constructorArguments: cobie,
+  });
 }
--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,17 +1,29 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import hre, { ethers } from "hardhat";
 
+function delay(ms: number) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
 async function main() {
-  const cobie = "0x4Cbe68d825d21cB4978F56815613eeD06Cf30152";
-  const contract = await ethers.deployContract("CobieEscrow", [cobie]);
+  const cobie = ["0x4Cbe68d825d21cB4978F56815613eeD06Cf30152"];
+  const contract = await ethers.deployContract("CobieEscrow", cobie);
 
   await contract.deployed();
+  const contractAddress = contract.address;
 
-  console.log("CobieEscrow deployed to:", contract.address);
+  console.log("CobieEscrow deployed to:", contractAddress);
+
+  await delay(30000); // Wait for 30 seconds before verifying the contract
+
+  await hre.run("verify:verify", {
+    address: contractAddress,
+    constructorArguments: cobie,
+  });
 
   // await hre.tenderly.verify({
   //   name: "CobieEscrow",
-  //   address: contract.address,
+  //   address: contractAddress,
   // });
 }
 
