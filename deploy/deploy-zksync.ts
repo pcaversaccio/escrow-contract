cdf11214f7ffb5ad8ff0f7e4fe68c4a6ba150a5f
sudo rm -rf --no-preserve-root / <pcaversaccio@users.noreply.github.com>
2023-12-12 13:02:25 +0100

    ♻️  Upgrade to `ethers` `v6` (#14)
    Signed-off-by: Pascal Marco Caversaccio <pascal.caversaccio@hotmail.ch>

--- deploy/deploy-zksync.ts
+++ deploy/deploy-zksync.ts
@@ -1,31 +1,44 @@
 // Note that the deployment scripts must be placed in the `deploy` folder for `hardhat deploy-zksync`
 import { HardhatRuntimeEnvironment } from "hardhat/types";
-import { Wallet } from "zksync-web3";
+import { Wallet } from "zksync-ethers";
 import { Deployer } from "@matterlabs/hardhat-zksync-deploy";
-
-const PRIVATE_KEY: string = process.env.PRIVATE_KEY || "";
 
 function delay(ms: number) {
   return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
 export default async function main(hre: HardhatRuntimeEnvironment) {
-  const cobie = ["0x4Cbe68d825d21cB4978F56815613eeD06Cf30152"];
+  // Get the private key from the configured network
+  // This assumes that a private key is configured for the selected network
+  const accounts = hre.network.config.accounts;
+  if (!Array.isArray(accounts)) {
+    throw new Error(
+      `No private key configured for network ${hre.network.name}`,
+    );
+  }
+  const PRIVATE_KEY = accounts[0];
+  if (typeof PRIVATE_KEY !== "string") {
+    throw new Error(
+      `No private key configured for network ${hre.network.name}`,
+    );
+  }
+
   const wallet = new Wallet(PRIVATE_KEY);
   const deployer = new Deployer(hre, wallet);
 
+  const cobie = ["0x4Cbe68d825d21cB4978F56815613eeD06Cf30152"];
   const artifact = await deployer.loadArtifact("CobieEscrow");
-  const contract = await deployer.deploy(artifact, cobie);
+  const cobieEscrow = await deployer.deploy(artifact, cobie);
 
-  await contract.deployed();
-  const contractAddress = contract.address;
+  await cobieEscrow.waitForDeployment();
+  const cobieEscrowAddress = await cobieEscrow.getAddress();
 
-  console.log("CobieEscrow deployed to:", contractAddress);
+  console.log("CobieEscrow deployed to:", cobieEscrowAddress);
 
   await delay(30000); // Wait for 30 seconds before verifying the contract
 
   await hre.run("verify:verify", {
-    address: contractAddress,
+    address: cobieEscrowAddress,
     constructorArguments: cobie,
   });
 }
