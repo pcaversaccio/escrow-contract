a1eec7af63ef2cda1ed4e32dc5a985187b07a55d
Pascal Marco Caversaccio <pascal.caversaccio@hotmail.ch>
2024-01-04 18:27:45 +0100

    ♻️ Update Base Sepolia Explorer Link and Bump `ethers` to Version `6.9.2`
    Signed-off-by: Pascal Marco Caversaccio <pascal.caversaccio@hotmail.ch>

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -875,8 +875,8 @@ const config: HardhatUserConfig = {
         network: "baseSepolia",
         chainId: 84532,
         urls: {
-          apiURL: "https://base-sepolia.blockscout.com/api",
-          browserURL: "https://base-sepolia.blockscout.com",
+          apiURL: "https://api-sepolia.basescan.org/api",
+          browserURL: "https://sepolia.basescan.org",
         },
       },
       {
