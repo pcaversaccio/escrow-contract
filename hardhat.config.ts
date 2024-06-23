60d3431cb2db1bf95d0cb60ddefcd66b4cf7bf12
Pascal Marco Caversaccio <pascal.caversaccio@hotmail.ch>
2024-06-23 14:47:15 +0200

    ♻️ Disable `ZKsync`
    Signed-off-by: Pascal Marco Caversaccio <pascal.caversaccio@hotmail.ch>

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -116,7 +116,7 @@ const config: HardhatUserConfig = {
         enabled: false,
       },
       ledgerAccounts,
-      zksync: true, // Enable ZKsync in the Hardhat local network
+      // zksync: true, // Enable ZKsync in the Hardhat local network
     },
     localhost: {
       url: "http://127.0.0.1:8545",
