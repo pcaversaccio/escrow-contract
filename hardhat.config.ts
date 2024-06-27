33aab51ef5891e182d30e11c302d74d7ea7bb54c
Pascal Marco Caversaccio <pascal.caversaccio@hotmail.ch>
2024-06-27 23:58:16 +0200

    ➕ Add Chiliz Spicy Test Network Configuration
    Signed-off-by: Pascal Marco Caversaccio <pascal.caversaccio@hotmail.ch>

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -759,6 +759,12 @@ const config: HardhatUserConfig = {
       accounts,
       ledgerAccounts,
     },
+    chilizTestnet: {
+      chainId: 88882,
+      url: vars.get("CHILIZ_TESTNET_URL", "https://spicy-rpc.chiliz.com"),
+      accounts,
+      ledgerAccounts,
+    },
   },
   xdeploy: {
     contract: "CobieEscrow",
@@ -937,6 +943,8 @@ const config: HardhatUserConfig = {
       // For Rootstock testnet & mainnet
       rootstock: vars.get("ROOTSTOCK_API_KEY", ""),
       rootstockTestnet: vars.get("ROOTSTOCK_API_KEY", ""),
+      // For Chiliz testnet
+      chilizTestnet: vars.get("CHILIZ_API_KEY", ""),
     },
     customChains: [
       {
@@ -1461,6 +1469,15 @@ const config: HardhatUserConfig = {
         urls: {
           apiURL: "https://rootstock-testnet.blockscout.com/api",
           browserURL: "https://rootstock-testnet.blockscout.com",
+        },
+      },
+      {
+        network: "chilizTestnet",
+        chainId: 88882,
+        urls: {
+          apiURL:
+            "https://api.routescan.io/v2/network/testnet/evm/88882/etherscan/api",
+          browserURL: "https://testnet.chiliscan.com",
         },
       },
     ],
