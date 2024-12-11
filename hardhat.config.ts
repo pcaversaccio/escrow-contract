78f7250ced784d96ef88a8ff55c77db237cd59c3
Pascal Marco Caversaccio <pascal.caversaccio@hotmail.ch>
2024-12-11 18:11:30 +0100

    ➕ Add Sonic Test and Main Network Configurations
    Signed-off-by: Pascal Marco Caversaccio <pascal.caversaccio@hotmail.ch>

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -80,7 +80,7 @@ const config: HardhatUserConfig = {
     },
   },
   zksolc: {
-    version: "1.5.7",
+    version: "1.5.8",
     compilerSource: "binary",
     settings: {
       enableEraVMExtensions: false,
@@ -954,6 +954,18 @@ const config: HardhatUserConfig = {
       accounts,
       ledgerAccounts,
     },
+    sonicTestnet: {
+      chainId: 57054,
+      url: vars.get("SONIC_TESTNET_URL", "https://rpc.blaze.soniclabs.com"),
+      accounts,
+      ledgerAccounts,
+    },
+    sonicMain: {
+      chainId: 146,
+      url: vars.get("SONIC_MAINNET_URL", "https://rpc.soniclabs.com"),
+      accounts,
+      ledgerAccounts,
+    },
   },
   xdeploy: {
     contract: "CobieEscrow",
@@ -1177,6 +1189,9 @@ const config: HardhatUserConfig = {
       superseedTestnet: vars.get("SUPERSEED_API_KEY", ""),
       // For Story testnet
       storyTestnet: vars.get("STORY_API_KEY", ""),
+      // For Sonic testnet & mainnet
+      sonic: vars.get("SONIC_API_KEY", ""),
+      sonicTestnet: vars.get("SONIC_API_KEY", ""),
     },
     customChains: [
       {
@@ -1951,6 +1966,22 @@ const config: HardhatUserConfig = {
         urls: {
           apiURL: "https://odyssey.storyscan.xyz/api",
           browserURL: "https://odyssey.storyscan.xyz",
+        },
+      },
+      {
+        network: "sonic",
+        chainId: 146,
+        urls: {
+          apiURL: "https://api.sonicscan.org/api",
+          browserURL: "https://sonicscan.org",
+        },
+      },
+      {
+        network: "sonicTestnet",
+        chainId: 57054,
+        urls: {
+          apiURL: "https://api-testnet.sonicscan.org/api",
+          browserURL: "https://testnet.sonicscan.org",
         },
       },
     ],
