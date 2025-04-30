f378bd02adf3b9c3911b0cc9393a057c8bcdec14
Pascal Marco Caversaccio <pascal.caversaccio@hotmail.ch>
2025-04-30 16:54:16 +0200

    🔁 Update `prettier-plugin-solidity` to Version `2.0.0`
    Signed-off-by: Pascal Marco Caversaccio <pascal.caversaccio@hotmail.ch>

--- contracts/CobieEscrow.sol
+++ contracts/CobieEscrow.sol
@@ -2,7 +2,9 @@
 pragma solidity 0.8.29;
 
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import {
+    SafeERC20
+} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import {AccessControl} from "@openzeppelin/contracts/access/AccessControl.sol";
 
 /////////////////////////////////////////////////////////////////
